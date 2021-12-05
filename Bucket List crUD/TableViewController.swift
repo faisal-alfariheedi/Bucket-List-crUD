7352ce16c00cea6ab7e7de93ff907c93c08fd7f3
faisal alfariheedi <falfariheedi@gmail.com>
2021-12-05 22:47:36 +0300

    fin

---
+++ Bucket List crUD/TableViewController.swift
@@ -0,0 +1,130 @@
+//
+//  TableViewController.swift
+//  Bucket List crUD
+//
+//  Created by faisal on 01/05/1443 AH.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController,cans {
+    
+    
+    @IBOutlet var table: UITableView!
+    
+    
+    var items = ["Sky diving", "Live in Hawaii"]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        table.dataSource=self
+        table.delegate=self
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if(segue.identifier == "add"){
+        let nv = segue.destination as! UINavigationController
+        let seg = nv.topViewController! as! ViewController
+        seg.owner=self
+        seg.dele=self
+            seg.ed=items.count
+        }else if (segue.identifier == "edit"){
+            let nv = segue.destination as! UINavigationController
+            let seg = nv.topViewController! as! ViewController
+            seg.owner=self
+            seg.dele=self
+            seg.ed=sender as! Int
+            seg.edit=true
+            seg.tex = items[sender as! Int]
+        }
+    }
+    func cancel(by cont: UIViewController) {
+        table.reloadData()
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+    // MARK: - Table view data source
+
+//    override func numberOfSections(in tableView: UITableView) -> Int {
+//        // #warning Incomplete implementation, return the number of sections
+//        return 0
+//    }
+//    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//
+//    }
+    override func tableView(_ tableView: UITableView, accessoryButtonTappedForRowWith indexPath: IndexPath) {
+        performSegue(withIdentifier: "edit", sender: indexPath.row )
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return items.count
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        items.remove(at: indexPath.row)
+        table.reloadData()
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = items[indexPath.row]
+        // Configure the cell...
+
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Bucket List crUD/ViewController.swift
+++ Bucket List crUD/ViewController.swift
@@ -7,13 +7,42 @@
 
 import UIKit
 
+protocol cans:class{
+    func cancel(by cont:UIViewController)
+}
+
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var add: UITextField!
+    var dele:cans?
+    var owner:UITableViewController?
+    var ed=0
+    var edit=false
+    var tex=""
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        add.text=tex
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func additem(_ sender: UIBarButtonItem) {
+        if(add.text!.isEmpty){}else{
+            if(edit){
+                (owner as! TableViewController).items[ed]=String(add.text!)
+                edit=false
+            }else{
+            (owner as! TableViewController).items.append(String(add.text!))
+            }
+                dele?.cancel(by: self)
+            print("shit")
+//            dismiss(animated: true, completion: nil)
+        }
+    }
+    
+    @IBAction func cancel(_ sender: UIBarButtonItem) {
+        dele?.cancel(by: self)
+//        dismiss(animated: true, completion: nil)
+    }
 }
 
